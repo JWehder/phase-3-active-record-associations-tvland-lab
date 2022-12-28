8260c742ccb2c2a6a746e3b4670e2ea0de87e430
Jake Wehder <jw@email.comjake>
2022-12-27 19:46:00 -0500

    commit tree 327db7a88623c682e04f125e9e1a8115d32fa130

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map do |character|
+        character.first_name + " " + character.last_name
+    end
+  end
+
 end
